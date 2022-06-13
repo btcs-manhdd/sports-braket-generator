21f6b85ff6583edca3347e2f0d689ced92626939
btcs-manhdd <manhdd@btc-studio.io>
2022-06-13 15:17:56 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from "react"
+import "./App.css"
+
+function App() {
+
+
+
+  return (
+    
+    <div className="App">
+      Hello
+      <h1>Hello nhasssad</h1>
+    </div>
+  )
+}
+
+export default App
